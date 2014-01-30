ad74809fea0e257301cc51af40217f94c7e2406a
Annie Jiao <anniej@mit.edu>
2014-01-29 23:12:36 -0500

    more progress on tests, still broken

--- test/nbody-test.cpp
+++ test/nbody-test.cpp
@@ -1,99 +1,10 @@
 #include <gtest/gtest.h>
-#include <Simulation.h>
-#include <System.h>
+#include <nbody/Simulation.h>
 
-#include <string>
 #include <iostream>
 #include <stdio.h>
-
-#include <dirent.h>
-
-const int MAX_CHARS_PER_LINE = 512;
 
 int main(int argc, char **argv ) {
     testing::InitGoogleTest(&argc, argv );
     return RUN_ALL_TESTS();
-}
-
-/* if dirent ends up not working out this is how to commandline bullshit */
-/* probably shouldn't use ever, muy bad */
-
-std::string exec(char* cmd) {
-    
-    /* cd into directory first */
-    
-    popen("cd", "r");
-    FILE* pipe = popen(cmd, "r");
-    if (!pipe) return "ERROR";
-    char buffer[128];
-    std::string result = "";
-    while(!feof(pipe)) {
-        if(fgets(buffer, 128, pipe) != NULL)
-            result += buffer;
-    }
-    pclose(pipe);
-    return result;
-}
-
-
-TEST(nbody-test, unibody-stationary) {
-    
-    try {
-        std::ifstream input{ "resources/nbody/unibody-stationary.txt" };
-        nbody::Simulation sim{input};
-        for(int i = 0; i < 40; ++i) {
-            std::cout << "==EVOLUTION " << i + 1 << "\n";
-            sim.saveRun();
-            
-            /* 1*10^10 steps, each dt constant size */
-            
-            sim.evolveSystem(1e10, 1);
-        }
-        sim.saveRun();
-        
-        //auto result = exec("ls");     use ls to get filename
-        
-        DIR *dir;
-        struct dirent *entry;
-        
-        if ((dir = opendir ("output path")) != NULL) { //still need output path
-
-            /* get dat file */
-            
-            std::string filename = entry->d_name;
-            
-            closedir (dir);
-        
-        } else {
-            
-            // well this sucks
-            perror ("error reading output file");
-            return EXIT_FAILURE;
-        }
-        
-        std::ifstream fin;
-        fin.open(filename + ".txt");
-        
-        while (!fin.eof()) {
-            
-            // this is kinda shitty and manual right now
-            // read each line and compare answers
-            
-            char line[MAX_CHARS_PER_LINE];
-            fin.getline(line, MAX_CHARS_PER_LINE);
-            ASSERT_STREQ(line, "1");
-            
-            fin.getline(line, MAX_CHARS_PER_LINE);
-            ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 2.0e+1");
-            
-            fin.getline(line, MAX_CHARS_PER_LINE);
-            ASSERT_STREQ(line, "");
-            
-        }
-        /* delete so the output directory only has one entry */
-        remove(filename + ".txt");
-    }
-    
-}
-
-    
+};
