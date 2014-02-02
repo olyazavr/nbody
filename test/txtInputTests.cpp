3991b6d44a283172955370acf47f00f6b9a78ff0
Annie Jiao <anniej@mit.edu>
2014-02-01 23:31:51 -0500

    Moar tests

--- test/txtInputTests.cpp
+++ test/txtInputTests.cpp
@@ -37,10 +37,10 @@ std::string exec(char* cmd) {
 }
 
 
-
+// One object at the center with no velocity or acceleration. Should not move.
 TEST(txtInputTests, stationary) {
     std::ifstream input{ "resources/nbody/unibody-stationary.txt" };
-    nbody::Simulation sim{input};
+    nbody::Simulation sim{ input };
     for(int i = 0; i < 40; ++i) {
         std::cout << "==EVOLUTION " << i + 1 << "\n";
         sim.saveRun();
@@ -48,13 +48,10 @@ TEST(txtInputTests, stationary) {
     }
     sim.saveRun();
     
-    string filename = "name";
     ifstream fin("name.txt");
     
     while (fin.is_open()) {
-        
-        // this is kinda shitty and manual right now
-        // read each line and compare answers
+
         char line[512];
         fin >> line;
         
@@ -69,5 +66,110 @@ TEST(txtInputTests, stationary) {
     }
     
 }
+
+// Six objects move toward center large mass
+
+TEST(txtInputTests, Centering) {
+    std::ifstream input{ "resources/nbody/perfectly-symmetric-distribution-move-toward-center.txt" };
+    nbody::Simulation sim{ input };
+    
+    for(int i = 0; i < 40; ++i) {
+        std::cout << "==EVOLUTION " << i + 1 << "\n";
+        sim.saveRun();
+        sim.evolveSystem(1e10, .001);       // make sure to get final state
+    }
+    
+    sim.saveRun();
+    
+    ifstream fin("name.txt");
+    
+    while (fin.is_open()) {
+        
+        char line[512];
+        fin >> line;
+        
+        ASSERT_STREQ(line, "7");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 2.0e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 2.0e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 2.0e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 2.0e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 2.0e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 2.0e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 1.0e+10");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "");
+        
+    }
+    
+}
+
+
+// Six objects converge towards center; seventh object has zero mass.
+
+TEST(txtInputTests, zeroMass) {
+    std::ifstream input{ "resources/nbody/centering-distribution-seven-zero-mass.txt" };
+    nbody::Simulation sim{ input };
+    
+    for(int i = 0; i < 40; ++i) {
+        std::cout << "==EVOLUTION " << i + 1 << "\n";
+        sim.saveRun();
+        sim.evolveSystem(1e10, .001);       // make sure to get final state
+    }
+    
+    sim.saveRun();
+    
+    ifstream fin("name.txt");
+    
+    while (fin.is_open()) {
+        
+        char line[512];
+        fin >> line;
+        
+        ASSERT_STREQ(line, "7");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 1.0e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 1.0e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 1.0e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 1.0e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 1.0e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 1.0e+1");
+        
+        fin >> line;
+        // The seventh object has a weird location/velocity/acceleration, but its mass is zero.
+        // Check to see that a zero mass doesn't break the system.
+        
+        fin >> line;
+        ASSERT_STREQ(line, "");
+        
+    }
+    
+}
+
 
 
