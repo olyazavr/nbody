ed3c2762fed9d2be7d62da14b9f0dd1e43689b79
Annie Jiao <anniej@mit.edu>
2014-02-01 23:48:33 -0500

    Additional cases tested

--- test/txtInputTests.cpp
+++ test/txtInputTests.cpp
@@ -149,5 +149,168 @@ TEST(txtInputTests, zeroMass) {
     
 }
 
+// BINARY SIMPLE
+// Tests the file provided
+
+TEST(txtInputTests, binarySimple) {
+    std::ifstream input{ "resources/nbody/binary-system-simple.txt" };
+    nbody::Simulation sim{ input };
+    
+    for(int i = 0; i < 40; ++i) {
+        sim.saveRun();
+        sim.evolveSystem(1e4, .001);       // make sure to get final state
+    }
+    
+    sim.saveRun();
+    
+    ifstream fin("name.txt");
+    
+    while (fin.is_open()) {
+        
+        char line[512];
+        fin >> line;
+        
+        ASSERT_STREQ(line, "2");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 1.0e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 1.0e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "");
+        
+    }
+    
+}
+
+// Approximately symmetric & stable objects
+// Six objects move toward center large mass
+
+TEST(txtInputTests, Centering) {
+    std::ifstream input{ "resources/nbody/approx-equal-centralize.txt" };
+    
+    /*
+     1.02342 0 -0.0023123  0 0 0  0 0.00078907896 0 2.05123124e+1
+     0 1.132123 0.0187807  0 0.0004351234 0  0 0 0 2.0356345723e+1
+     -0.00806789 0 1.348568  0 0 0  0 0 -0.0045235 2.067678956e+1
+     0.003452 -0.008 -1.85985678  0 -0.0003452435 0  0 0 0 2.01234565e+1
+     0.00786780 -1.45674567 0.01312  0.0002131234 0 0  0.0003541235 0 0 2.03658578e+1
+     -1.6785634 -0.01234123 0.03456123  0.001243123 0 0  0 -0.00131234 0 2.0356657e+1
+     0.0005678456 -0.000025236 0.00004657345  0 0.00345123 0  0.01123452346 0 0 2.034256345e+10
+
+     */
+    
+    
+    nbody::Simulation sim{ input };
+    
+    for(int i = 0; i < 40; ++i) {
+        sim.saveRun();
+        sim.evolveSystem(1e5, .001);       // make sure to get final state
+    }
+    
+    sim.saveRun();
+    
+    ifstream fin("name.txt");
+    
+    while (fin.is_open()) {
+        
+        char line[512];
+        fin >> line;
+        
+        ASSERT_STREQ(line, "7");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 2.05123124e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 2.0356345723e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 2.067678956e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 2.01234565e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 2.03658578e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 2.0356657e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "0 0 0  0 0 0  0 0 0 2.034256345e+10");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "");
+        
+    }
+    
+}
+
+// Shift a converge by +10.5, +10.5, +10.5, make sure it's not just showing 0 0 0 every time
+
+TEST(txtInputTests, convergeShifted) {
+    
+    /*
+     
+     7
+     11.5 10.5 10.5  0 0 0  0 0 0 2.0e+1
+     10.5 11.5 10.5  0 0 0  0 0 0 2.0e+1
+     10.5 10.5 11.5  0 0 0  0 0 0 2.0e+1
+     10.5 10.5 9.5  0 0 0  0 0 0 2.0e+1
+     10.5 9.5 10.5  0 0 0  0 0 0 2.0e+1
+     9.5 10.5 10.5  0 0 0  0 0 0 2.0e+1
+     10.5 10.5 10.5  0 0 0  0 0 0 1.0e+10
+
+     */
+    std::ifstream input{ "resources/nbody/converge-shifted.txt" };
+    nbody::Simulation sim{ input };
+    
+    for(int i = 0; i < 40; ++i) {
+        //sim.saveRun();
+        sim.evolveSystem(1e5, .001);       // make sure to get final state
+    }
+    
+    sim.saveRun();
+    
+    ifstream fin("name.txt");
+    
+    while (fin.is_open()) {
+        
+        char line[512];
+        fin >> line;
+        
+        ASSERT_STREQ(line, "7");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "10.5 10.5 10.5  0 0 0  0 0 0 2.0e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "10.5 10.5 10.5  0 0 0  0 0 0 2.0e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "10.5 10.5 10.5  0 0 0  0 0 0 2.0e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "10.5 10.5 10.5  0 0 0  0 0 0 2.0e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "10.5 10.5 10.5  0 0 0  0 0 0 2.0e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "10.5 10.5 10.5  0 0 0  0 0 0 2.0e+1");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "10.5 10.5 10.5  0 0 0  0 0 0 1.0e+10");
+        
+        fin >> line;
+        ASSERT_STREQ(line, "");
+    }
+    
+}
+
+
 
 
